62900f0d732a9c0d42217bc8dc534b9c460c2afd
GiovanniBurungHR <1096110@hr.nl>
2025-03-13 15:00:50 +0100

    Test Cube

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+const HOST = '145.24.222.149';
+
+// Serve static files (HTML, JS, CSS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running at http://${HOST}:${PORT}`);
+});
